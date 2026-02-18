0a9be83301964f8dad64dd2c8575119861dad71b
TJS-DIY-Creations <tylerbjohannsen@gmail.com>
2026-02-18 13:47:27 -0800

    Update rgb_led_catch_bot_v5.ino

--- rgb_led_catch_bot_v5.ino
+++ rgb_led_catch_bot_v5.ino
@@ -1,29 +1,28 @@
-#include <AccelStepper.h>
+#include <AccelStepper.h>  // include libs
 #include <math.h>
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 
-// ============================================================
 // OLED
-// ============================================================
 #define SCREEN_WIDTH 128
 #define SCREEN_HEIGHT 64
 #define OLED_RESET -1
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
 // define rgb led pins
-int redPin = 11; 
+int redPin = 11;
 int greenPin = 10;
 int bluePin = 9;
 
+//color structure
 struct Color {
   int r;
   int g;
   int b;
 };
 
-// Bigger color list
+// rgb main color list
 Color colors[] = {
   { 255, 0, 0 },   // Red
   { 255, 40, 0 },  // Red-Orange
@@ -50,53 +49,50 @@ Color colors[] = {
   { 180, 200, 255 }   // Cool white
 };
 
-const int colorCount = sizeof(colors) / sizeof(colors[0]);
+const int colorCount = sizeof(colors) / sizeof(colors[0]);  // finds amount of differnet colors
 
 // Timing
 unsigned long previousMillis = 0;
 const long fadeInterval = 10;
 const int fadeSteps = 100;
 
-// State
+// rgb color variables
 int currentIndex = 0;
 int nextIndex = 1;
 int stepCount = 0;
 
-float version = 1.1;
+float version = 5;  // code version
 
-const byte sens1 = 2;//sensor pins
+const byte sens1 = 2;  //sensor pins
 const byte sens2 = 3;
 
 const float distanceMeters = 0.02815;  //distance between sensors, in cad 0.02815
-const unsigned long debounceUS = 50; // debounce to prevent false trigger
+const unsigned long debounceSense = 50;   // very small debounce to prevent false trigger
 
 //variables
-const int stepsPerCM = 50; // steps to move 1 cm
-float h = 0.825; //hight of desk
-float g = 9.81; // gravity
+const int stepsPerCM = 50;  // steps to move 1 cm
+float h = 0.825;            //hight of desk
+float g = 9.81;             // gravity
 
 // A4988 pins
 #define STEP_PIN 4
 #define DIR_PIN 5
 #define ENABLE_PIN 6
 
-AccelStepper stepper(AccelStepper::DRIVER, STEP_PIN, DIR_PIN); //defines stepper driver pins
+AccelStepper stepper(AccelStepper::DRIVER, STEP_PIN, DIR_PIN);  //defines stepper driver pins
 
 // ISR RESULTS
 volatile unsigned long tStart = 0;
 volatile unsigned long tStop = 0;
 volatile bool measurementReady = false;
 
-// ============================================================
 // CALCULATED VALUES
-// ============================================================
+
 float speed_mps = 0.0;
 float speed_fps = 0.0;
 long targetSteps = 0;
 
-// ============================================================
-// STATE MACHINE
-// ============================================================
+// difine different states the catch bot can be in
 enum SystemState {
   ARMED,
   CALCULATING,
@@ -105,27 +101,27 @@ enum SystemState {
   WAITING_FOR_CLEAR
 };
 
-SystemState currentState = ARMED;
+SystemState currentState = ARMED;  // catch bot ready or not ready
 
-unsigned long moveCompleteMillis = 0;
-const unsigned long returnDelay = 2000;
+unsigned long moveCompleteMillis = 0; // time it finished moving
+const unsigned long returnDelay = 2000; // delay before motor returns to set 0 position
 
 // INTERRUPTS
-void sens1ISR() {
+void sens1ISR() { // finds the start time of the first ensor trigger 
   static unsigned long last = 0;
-  unsigned long now = micros();
+  unsigned long now = micros(); 
 
-  if (now - last > debounceUS) {
+  if (now - last > debounceSense) {
     tStart = now;
   }
   last = now;
 }
 
-void sens2ISR() {
+void sens2ISR() { // finds the time of the second sensor trigger
   static unsigned long last = 0;
   unsigned long now = micros();
 
-  if (now - last > debounceUS && tStart != 0) {
+  if (now - last > debounceSense && tStart != 0) {
     tStop = now;
     measurementReady = true;
   }
@@ -136,27 +132,27 @@ void sens2ISR() {
 // SETUP
 
 void setup() {
-  Serial.begin(115200);
-  Wire.begin();
+  // Serial.begin(115200);
+  Wire.begin();  // starts the wire lib to comunicate with display over the scl/sda pins useing 12ic
 
-  if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
+  if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {  // starts the display
     for (;;)
       ;
   }
 
-  pinMode(sens1, INPUT);
+  pinMode(sens1, INPUT);  //sets sensors as inputs
   pinMode(sens2, INPUT);
 
-  pinMode(ENABLE_PIN, OUTPUT);
-  digitalWrite(ENABLE_PIN, LOW);
+  //pinMode(ENABLE_PIN, OUTPUT); // not required
+  //digitalWrite(ENABLE_PIN, LOW);
 
-  pinMode(redPin, OUTPUT);
+  pinMode(redPin, OUTPUT);  // set rgb led pins as outputs
   pinMode(greenPin, OUTPUT);
   pinMode(bluePin, OUTPUT);
 
-  stepper.setMaxSpeed(250000);
+  stepper.setMaxSpeed(250000);  // set max speed/acceleration
   stepper.setAcceleration(250000);
-  stepper.setCurrentPosition(0);
+  stepper.setCurrentPosition(0);  // set zero asuming correctly alinged with end of table
 
   attachInterrupt(digitalPinToInterrupt(sens1), sens1ISR, RISING);
   attachInterrupt(digitalPinToInterrupt(sens2), sens2ISR, RISING);
@@ -165,128 +161,140 @@ void setup() {
 }
 
 void loop() {
-  stepper.run();
+  stepper.run();  //accel stepper requires this to be called every loop
 
   switch (currentState) {
 
     case ARMED:
+      // wait until the sensors detect something passing through
       if (measurementReady) currentState = CALCULATING;
       break;
 
     case CALCULATING:
       {
-        noInterrupts();
+        noInterrupts();  // pause interrupts so values dont change mid calc
         unsigned long start = tStart;
         unsigned long stop = tStop;
-        tStart = 0;
+        tStart = 0;  // reset time of start for next run
         measurementReady = false;
-        interrupts();
+        interrupts();  // turn interrupts back on
 
-        unsigned long dt = stop - start;
-        if (dt == 0) dt = 1;
+        unsigned long dt = stop - start;  // calculate time difference
+        if (dt == 0) dt = 1;              // prevent dividing by zero if sensors glitch
 
+        // calculate meters per second
         speed_mps = distanceMeters / (dt / 1000000.0);
-        speed_fps = speed_mps * 3.28084;
+        speed_fps = speed_mps * 3.28084;  // convert to feet per second
 
+        // projectile motion math to find where it will land
         float X = speed_mps * sqrt((2.0 * h) / g);
-        float X_cm = X * 100.0;
-        targetSteps = (long)(X_cm * stepsPerCM);
+        float X_cm = X * 100.0;                   // convert meters to cm
+        targetSteps = (long)(X_cm * stepsPerCM);  // convert cm to motor steps
 
+        // if it has a valid amount of target steps it will tell the stepper to move
         if (targetSteps > 0) {
-          stepper.moveTo(targetSteps);
-          currentState = MOVING;
+          stepper.moveTo(targetSteps);  // moves stepper
+          currentState = MOVING;        // tell code its curently moveing to reduce crashes of rail system
         } else {
           currentState = WAITING_FOR_CLEAR;
         }
 
-        drawDisplay();
+        drawDisplay();  // update screen with new speeds and steps
         break;
       }
 
     case MOVING:
+      // check if the motor has finished reaching the position
       if (stepper.distanceToGo() == 0) {
-        moveCompleteMillis = millis();
+        moveCompleteMillis = millis();  // mark the time finished moveing
         currentState = RETURNING;
       }
       break;
 
     case RETURNING:
+      // wait a bit so we actually catch the object before moving back
       if (millis() - moveCompleteMillis >= returnDelay) {
-        stepper.moveTo(0);
+        stepper.moveTo(0);  // sends the track back to 0 position
         currentState = WAITING_FOR_CLEAR;
       }
       break;
 
     case WAITING_FOR_CLEAR:
+      // make sure motor is home and sensors are not blocked before resetting
       if (stepper.distanceToGo() == 0 && digitalRead(sens1) == LOW && digitalRead(sens2) == LOW) {
 
-        delay(300);
-        currentState = ARMED;
+        delay(300);            // small buffer for stability
+        currentState = ARMED;  // ready for the next marble
         drawDisplay();
       }
       break;
   }
+
+  // RGB FADING
   unsigned long currentMillis = millis();
 
-  if (currentMillis - previousMillis >= fadeInterval) { 
+  if (currentMillis - previousMillis >= fadeInterval) {  // fades to next color after the fade interval or the delay betwn color changes
     previousMillis = currentMillis;
 
+    // calculate the blend between the current color and the next one
     float t = (float)stepCount / fadeSteps;
 
-    // changes the color of the rgb leds
+    // math to interpolate the r, g, b values
     int r = colors[currentIndex].r + (colors[nextIndex].r - colors[currentIndex].r) * t;
     int g = colors[currentIndex].g + (colors[nextIndex].g - colors[currentIndex].g) * t;
     int b = colors[currentIndex].b + (colors[nextIndex].b - colors[currentIndex].b) * t;
 
-    setColor(r, g, b);
+    setColor(r, g, b);  // apply the faded color to the pins
 
     stepCount++;
 
+    // if fade is finished, move to the next color in the list
     if (stepCount > fadeSteps) {
       stepCount = 0;
       currentIndex = nextIndex;
-      nextIndex = (nextIndex + 1) % colorCount;
+      nextIndex = (nextIndex + 1) % colorCount;  // loop back to start of array
     }
   }
 }
 
-void drawDisplay() { //updates the display
-  display.clearDisplay(); // clears display
 
-  display.setTextColor(WHITE); // sets text color but doesnt really work due to display hardware limitations
+void drawDisplay() {       //updates the display
+  display.clearDisplay();  // clears display
+
+  display.setTextColor(WHITE);  // sets text color but doesnt really work due to display hardware limitations
 
   // version
-  display.setTextSize(1); // change text size
-  display.setCursor(0, 0); // move cursor
-  display.print("v "); // prints v for "version"
-  display.println(version, 1); // prints code version next to v
+  display.setTextSize(1);       // change text size
+  display.setCursor(0, 0);      // move cursor
+  display.print("v ");          // prints v for "version"
+  display.println(version, 1);  // prints code version next to v
 
   // speeds
-  display.setTextSize(2); // sets to bigger text size
-  display.setCursor(0, 16); // moves cursor
-  display.print(speed_mps, 1); // then prints the m/s next to it 
-  display.print("m/s"); // prints m/s units
+  display.setTextSize(2);       // sets to bigger text size
+  display.setCursor(0, 16);     // moves cursor
+  display.print(speed_mps, 1);  // then prints the m/s next to it
+  display.print("m/s");         // prints m/s units
 
-  display.setCursor(0, 36); // moves cursor again
-  display.print(speed_fps, 0); // prints speed in foot per second, why did i add this??????
-  display.print("fps"); // prints units
+  display.setCursor(0, 36);     // moves cursor again
+  display.print(speed_fps, 0);  // prints speed in foot per second, why did i add this??????
+  display.print("fps");         // prints units
 
   // steps
-  display.setTextSize(1); // changes text size back
-  display.setCursor(80, 0); //moves cursor
-  display.print("Steps:"); // label
-  display.setCursor(80, 10); // moves cursor again
-  display.print(targetSteps); // prints steps that the motor moved 
+  display.setTextSize(1);      // changes text size back
+  display.setCursor(80, 0);    //moves cursor
+  display.print("Steps:");     // label
+  display.setCursor(80, 10);   // moves cursor again
+  display.print(targetSteps);  // prints steps that the motor moved
 
   // state
-  display.setCursor(80, 25); //moves cursor again
-  display.print("State:"); // prints label
-  display.setCursor(80, 35); // moves cursor 
-  display.print(currentState); // prints state
+  display.setCursor(80, 25);    //moves cursor again
+  display.print("State:");      // prints label
+  display.setCursor(80, 35);    // moves cursor
+  display.print(currentState);  // prints state
 
-  display.display(); // send all above to display 
+  display.display();  // send all above to display
 }
-void setColor(int redValue, int greenValue, int blueValue) { // rgb code
+void setColor(int redValue, int greenValue, int blueValue) {  // rgb code
   analogWrite(redPin, redValue);
   analogWrite(greenPin, greenValue);
   analogWrite(bluePin, blueValue);
